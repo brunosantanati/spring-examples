0798f54f24036b61188b0ef2761244ba14d1e9eb
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-04 12:08:04 -0300

    Habilitar Feign Logs para debugar requests.

--- feign-example/src/main/kotlin/com/example/feign/example/FeignExampleApplication.kt
+++ feign-example/src/main/kotlin/com/example/feign/example/FeignExampleApplication.kt
@@ -1,13 +1,25 @@
 package com.example.feign.example
 
+import feign.Logger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = ["com.example.feign.example"])
 @SpringBootApplication
-class FeignExampleApplication
+class FeignExampleApplication {
 
-fun main(args: Array<String>) {
-	runApplication<FeignExampleApplication>(*args)
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<FeignExampleApplication>(*args)
+		}
+
+		@Bean
+		fun feignLoggerLevel(): Logger.Level? {
+			return Logger.Level.FULL
+		}
+	}
+
 }
