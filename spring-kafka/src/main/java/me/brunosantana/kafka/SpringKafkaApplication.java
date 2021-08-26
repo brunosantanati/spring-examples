9391278c86b24a1cde5914207d3d34749b00f558
Bruno Sant' Ana <Bruno.Ana@br.experian.com>
2021-08-26 17:41:37 -0300

    Exemplo de producer do Kafka usando KafkaTemplate.

---
+++ spring-kafka/src/main/java/me/brunosantana/kafka/SpringKafkaApplication.java
@@ -0,0 +1,36 @@
+package me.brunosantana.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+//Exemplo baseado em: https://www.baeldung.com/spring-kafka
+//Sobre CommandLineRunner:
+//https://receitasdecodigo.com.br/spring-boot/spring-boot-usando-applicationrunner-e-commandlinerunner
+//https://jhooq.com/commandlinerunner-spring-boot/
+
+@SpringBootApplication
+public class SpringKafkaApplication implements CommandLineRunner {
+
+	@Value("${kafka.topic}")
+	private String topicName;
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		sendMessage("Minha mensagem");
+	}
+
+	public void sendMessage(String msg) {
+		kafkaTemplate.send(topicName, msg);
+	}
+}
