af589fb4e12691381396e709a515a172ac8a3c79
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-01-26 16:30:10 -0300

    Usando @TestConfiguration e @Import para configurar Bean do teste.

--- general-examples/src/main/java/me/brunosantana/config/TestNameConfig.java
+++ general-examples/src/main/java/me/brunosantana/config/TestNameConfig.java
@@ -3,22 +3,25 @@ package me.brunosantana.config;
 import me.brunosantana.utils.NameGenerator;
 import me.brunosantana.utils.RandomNameGenerator;
 import me.brunosantana.utils.TestNameGenerator;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-public class NameConfig {
+@TestConfiguration
+public class TestNameConfig {
+
+    //@TestConfiguration and @Import:
+    //https://mkyong.com/spring-boot/spring-boot-how-to-init-a-bean-for-testing/
 
     //Use the VM argument: -Dspring.profiles.active=test
     //https://www.baeldung.com/spring-profiles
 
     @Bean
-    //@Primary
-    @Profile("!test")
-    public NameGenerator nameGenerator(){
-        return new RandomNameGenerator();
+    @Profile("test")
+    public NameGenerator nameGeneratorTest(){
+        return new TestNameGenerator();
     }
+
 
 }
--- general-examples/src/test/java/me/brunosantana/service/MyServiceTest.java
+++ general-examples/src/test/java/me/brunosantana/service/MyServiceTest.java
@@ -1,14 +1,17 @@
 package me.brunosantana.service;
 
+import me.brunosantana.config.TestNameConfig;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Import(TestNameConfig.class)
 public class MyServiceTest {
 
     @Autowired
