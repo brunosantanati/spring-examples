10156728051869f0023df5e22a5ddccd32140d7d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-01-26 13:27:16 -0300

    Projeto para propositos gerais (testar diversas coisas do Spring).

---
+++ general-examples/src/test/java/me/brunosantana/GeneralExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunosantana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
