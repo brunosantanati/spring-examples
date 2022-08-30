756998f8761fd04029d675a6f8e15b2e84b9d840
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-08-30 16:08:09 -0300

    Retry for more than 1 exception type

--- sprint-retry-kotlin/src/main/kotlin/me/brunosantana/service/RetryService.kt
+++ sprint-retry-kotlin/src/main/kotlin/me/brunosantana/service/RetryService.kt
@@ -8,18 +8,21 @@ import java.sql.SQLException
 @Service
 class RetryService {
 
-    @Retryable(value = [SQLException::class])
+    @Retryable(value = [SQLException::class, RuntimeException::class])
     fun retryServiceWithRecovery(sql: String) {
         println("\nexecuting...")
         val randomNumber = (0..1).random()
         if(randomNumber == 0) {
             throw SQLException("error in the select statement"); //only retry if the exception is a SQLException
         }
+        if(randomNumber == 1) {
+            throw RuntimeException("problem with the select statement"); //only retry if the exception is a SQLException
+        }
         print("OK")
     }
 
     @Recover
-    fun recover(e: SQLException, sql: String){
+    fun recover(e: Exception, sql: String){
         println("recovering... - ${e.message} - $sql")
     }
 
