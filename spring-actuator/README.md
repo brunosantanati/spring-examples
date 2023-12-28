6d564824ed22a0b67937be221c786b14daf6fbb6
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-12-28 19:45:26 -0300

    Empty project for Actuator

---
+++ spring-actuator/README.md
@@ -0,0 +1,3 @@
+## Useful Links
+
+[Health Indicators in Spring Boot](https://www.baeldung.com/spring-boot-health-indicators)    
---
+++ spring-actuator/src/test/java/me/brunosantana/springactuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunosantana.springactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
